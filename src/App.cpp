7c729a276005371a90892f7396106b50f5f6809b
Dream-Reality <1282852686@qq.com>
2023-11-20 16:18:45 +0800

    Update App.cpp

--- src/App.cpp
+++ src/App.cpp
@@ -1,4 +1,3 @@
-
 #include "websocket/WSListener.hpp"
 
 #include "oatpp/parser/json/mapping/ObjectMapper.hpp"
@@ -29,23 +28,33 @@ namespace {
         OATPP_LOGD(TAG, "SOCKET CLOSED!!!");
         finished = true;
     }
-
 }
 
 void run() {
     OATPP_LOGI(TAG, "Application Started");
     INTERFACE_SPACE::init();
     auto jsonObjectMapper = oatpp::parser::json::mapping::ObjectMapper::createShared();
+    // 请在这里输入参赛选手的账号和密码
     auto bot = BOTS_SPACE::BotsDemoClass("UBIQ_USER", "123456");
     bot.login();
     bot.init();
-    for (int i = 0;i < bot.running_days; i++){
+    int SimTimeLen = 14400;
+    while (true){
+        auto t = UTIL_SPACE::ConvertToSimTime_us(bot.start_time, bot.time_ratio, bot.day, bot.running_time);
+        if (t < SimTimeLen){
+            break;
+        }
+        bot.day += 1;
+    }
+    while (bot.day < bot.running_days){
         while (true){
             auto t = UTIL_SPACE::ConvertToSimTime_us(bot.start_time, bot.time_ratio, bot.day, bot.running_time);
             if (t > -900)break;
         }
         bot.bod();
-        for (double s = 0.; s < 14400; s += 1.){
+        // 此处demo为：从当前时刻开始，之后每一秒执行一次work()，一直到交易所收盘。可根据程序的速度及延迟进行调整。
+        auto now = UTIL_SPACE::ConvertToSimTime_us(bot.start_time, bot.time_ratio, bot.day, bot.running_time);
+        for (double s = now; s < SimTimeLen; s += 1.){
             while (true){
                 auto t = UTIL_SPACE::ConvertToSimTime_us(bot.start_time, bot.time_ratio, bot.day, bot.running_time);
                 if (t >= s){
@@ -55,6 +64,7 @@ void run() {
             bot.work();
         }
         bot.eod();
+        bot.day += 1;
     }
 }
 
