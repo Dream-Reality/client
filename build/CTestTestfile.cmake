c05123a2bb8da5698412fe57352b656dc9c05ff3
Dream-Reality <1282852686@qq.com>
2023-11-20 16:22:00 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/server/oatpp/src/client_user
-# Build directory: /root/server/oatpp/src/client_user/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "websocket-client-test")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/root/server/oatpp/src/client_user/CMakeLists.txt;53;add_test;/root/server/oatpp/src/client_user/CMakeLists.txt;0;")
